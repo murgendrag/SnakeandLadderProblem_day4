398f87a6663c3c5e3fed14f05f825f3212fd906c
murgendrag <murgendra67@gmail.com>
2022-03-02 17:29:44 +0530

    start position

--- rollingdie.cs
+++ rollingdie.cs
@@ -8,11 +8,15 @@ namespace SnakeandLaddergame
 {
      class rollingdie
     {
+        int initialPosition=0;
+        int maxPosition=100;
+        int playerPosition=0;
         public static void rolldie()
         {
             Random random = new Random();
             int die=random.Next(1,7);
             Console.WriteLine("number on die :" + die);
+
         }
     }
 }

5325c6be71b89fb198f2ddc24206cce4d919da28
murgendrag <murgendra67@gmail.com>
2022-03-02 18:13:32 +0530

    use of case statement

--- rollingdie.cs
+++ rollingdie.cs
@@ -8,15 +8,42 @@ namespace SnakeandLaddergame
 {
      class rollingdie
     {
-        int initialPosition=0;
-        int maxPosition=100;
-        int playerPosition=0;
-        public static void rolldie()
+        //public const int ladder = 1,3,5;
+        public static void rolldie( )
         {
-            Random random = new Random();
+           
+            int playerPosition = 0;
+
+        Random random = new Random();
             int die=random.Next(1,7);
             Console.WriteLine("number on die :" + die);
 
+            switch (die)
+            {
+                default:
+                    playerPosition =0; // at start
+                    break;
+                case 1 :           //ladder at 1,3,5
+                    playerPosition = playerPosition + die;
+                    break;
+                case 2:             //snake at 2,4,6
+                    playerPosition= playerPosition + die;
+                    break;
+                case 3:
+                    playerPosition = playerPosition + die;
+                    break;
+                case 4:
+                    playerPosition = playerPosition - die;
+                    break;
+                case 5:
+                    playerPosition = playerPosition + die;
+                    break;
+                case 6:
+                    playerPosition = playerPosition -die;
+                    break;
+                     
+            }
+            Console.WriteLine("player position :" + playerPosition);
         }
     }
 }

cfbee07d462c150824e55a54a7dbe24b642b6100
murgendrag <murgendra67@gmail.com>
2022-03-02 17:16:50 +0530

    random function used

---
+++ rollingdie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLaddergame
+{
+     class rollingdie
+    {
+        public static void rolldie()
+        {
+            Random random = new Random();
+            int die=random.Next(1,7);
+            Console.WriteLine("number on die :" + die);
+        }
+    }
+}
